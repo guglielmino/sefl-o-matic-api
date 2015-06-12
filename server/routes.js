bbdc7ab79dca9c674a23f900a17d95683c7b7202
Fabrizio Guglielmino <guglielmino@gmail.com>
2015-06-12 10:23:56 +0200

    Fix routes initialialization

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,13 @@
 
 'use strict';
 
-module.exports = function(app) {
+module.exports = function(app, storageProvider) {
 
-  // Insert routes below
-  app.use('/api/machines', require('./api/machines'));
+  var machinesProvider = storageProvider.machinesProvider();
+  var machinesController = require('./api/machines')(machinesProvider);
+ 
+  // Routes
+  app.use('/api/machines', machinesController);
   
    app.route('/:url(api|auth|components|app|bower_components|assets|static)/*').get(function(req, res) {
     return res.status(404).send({
