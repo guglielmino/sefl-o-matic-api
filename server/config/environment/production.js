1092d16cccdafe27561ba385a1c6382936fb6c05
Fabrizio Guglielmino <guglielmino@gmail.com>
2015-09-17 17:51:06 +0200

    Modified production config to support docker

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,15 +4,15 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.IP ||
-            undefined,
+  ip: process.env.IP ||
+    undefined,
 
   // Server port
-  port:     process.env.PORT ||
-            8080,
+  port: process.env.PORT ||
+    8080,
 
   // MongoDB connection options
   mongo: {
-    uri:  'mongodb://localhost/selfomaticapi'
+    uri: 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost:' + process.env.MONGO_PORT_27017_TCP_PORT || '27017' + '/selfomaticapi'
   }
 };
