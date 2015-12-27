e01f2da2805549ee395f246a70f7b7e0300c08f4
Fabrizio <guglielmino@gmail.com>
2015-12-27 23:49:00 +0100

    Fix in login controller

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -2,37 +2,42 @@
     'use strict';
 
     angular.module('somapp.account.login')
-        .controller('LoginCtrl', ['$rootScope', 'Auth', '$location', '$window',
-            function ($rootScope, Auth, $location, $window) {
-                var self = this;
-                self.user = {};
-                self.errors = {};
+        .controller('LoginCtrl',
+            [
+                '$rootScope',
+                'Auth',
+                '$location',
+                '$window',
+                function ($rootScope, Auth, $location, $window) {
+                    var self = this;
+                    self.user = {};
+                    self.errors = {};
 
-                $rootScope.areaTitle = 'Self-O-Matic Login';
+                    $rootScope.areaTitle = 'Self-O-Matic Login';
 
-                self.login = function (userForm) {
-                    self.submitted = true;
+                    self.login = function (loginForm) {
+                        self.submitted = true;
 
-                    if (userForm.$valid) {
+                        if (loginForm.$valid) {
 
-                        Auth.login({
-                                email: self.user.username,
-                                password: self.user.password
-                            })
-                            .then(function () {
-                                // Logged in, redirect to home or target url
-                                var targetUrl = $location.search().targetUrl || '/';
-                                $location.path(targetUrl);
-                            })
-                            .catch(function (err) {
+                            Auth.login({
+                                    email: self.user.username,
+                                    password: self.user.password
+                                })
+                                .then(function () {
+                                    // Logged in, redirect to home or target url
+                                    var targetUrl = $location.search().targetUrl || '/';
+                                    $location.path(targetUrl);
+                                })
+                                .catch(function (err) {
 
-                                self.errors.other = err.message;
-                            });
-                    }
-                };
+                                    self.errors.other = err.message;
+                                });
+                        }
+                    };
 
-                self.loginOauth = function (provider) {
-                    $window.location.href = '/auth/' + provider;
-                };
-            }]);
+                    self.loginOauth = function (provider) {
+                        $window.location.href = '/auth/' + provider;
+                    };
+                }]);
 })();
